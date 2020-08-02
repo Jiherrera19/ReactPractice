20b79ef4e377cfeb84fae6afe19b38adc061fc28
Joshua Herrera <joshua.herrera@capitalone.com>
2020-08-02 18:10:55 -0500

    Finished Login App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Base Application</h1>
-    </div>
-  );
+import Button from 'react-bootstrap/Button'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false
+    };
+    this.buttonClicked = this.buttonClicked.bind(this);
+  }
+  buttonClicked() {
+    this.setState(state => {
+      return {
+        loggedIn: !state.loggedIn
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>You are: {this.state.loggedIn ? "Logged In" : "Logged Out"}</h1>
+        <Button onClick={this.buttonClicked}>{this.state.loggedIn ? "Log Out" : "Log In"}</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
