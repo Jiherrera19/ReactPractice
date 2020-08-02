8be8bbc7fd135bc87c5ee6bf855aa00ef50071ec
Joshua Herrera <joshua.herrera@capitalone.com>
2020-08-02 18:19:02 -0500

    Styling Changes to check and unchecked text

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -5,11 +5,16 @@ import Form from 'react-bootstrap/Form';
 
 class ChecklistItem extends React.Component {
     render() {
+        const stylesCompleted = {
+            color: "gray",
+            textDecoration: "line-through",
+            fontStyle: "italic"
+        }
         return (
         <ListGroup.Item>
             <Form.Check type="checkbox" id={this.props.todo.id} custom>
-                <Form.Check.Input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handler(this.props.todo.id)}/>
-                <Form.Check.Label className="active">{this.props.todo.text}</Form.Check.Label>
+                <Form.Check.Input checked={this.props.todo.completed} onChange={() => this.props.handler(this.props.todo.id)}/>
+                <Form.Check.Label style = {this.props.todo.completed ? stylesCompleted : null} type="checkbox" className="active">{this.props.todo.text}</Form.Check.Label>
             </Form.Check>
         </ListGroup.Item>
         );
