fb9b4b75864ec942cf0d5f7560a5dad5aeab043d
Joshua Herrera <joshua.herrera@capitalone.com>
2020-08-04 22:34:15 -0500

    Changes to how css is handled

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Container, Image } from 'react-bootstrap';
 
-import './Meme.css';
-
 class Meme extends React.Component {
     render() {
+        const meme = {
+            margin: "auto",
+            textAlign: "center",
+            position: "relative",
+            backgroundSize: "100%"
+          };
+        const h2 = {
+            position: "absolute",
+            left: 0,
+            right: 0,
+            margin: "15px 0",
+            padding: "0 5px",
+            fontFamily: "impact",
+            fontSize: "2.5em",
+            textTransform: 'uppercase',
+            color: "white",
+            letterSpacing: "1px",
+            textShadow: `2px 2px 0 #000,
+            -2px -2px 0 #000,
+            2px -2px 0 #000,
+            -2px 2px 0 #000,
+            0px 2px 0 #000,
+            2px 0px 0 #000,
+            0px -2px 0 #000,
+            -2px 0px 0 #000,
+            2px 2px 5px #000`
+          };
+
+        const top = {
+            top: "25px",
+            ...h2
+        }
+
+        const bottom = {
+            bottom: "15px",
+            ...h2
+        }
         return (
             <>
-                <Container className="meme">
+                <Container style={meme}>
                     <Image style={{maxWidth: "50%"}} src={this.props.randomImage} alt=""/>
-                    <h2 className="top">{this.props.topText}</h2>
-                    <h2 className="bottom">{this.props.bottomText}</h2>
+                    <div style={top}>{this.props.topText}</div>
+                    <div style={bottom}>{this.props.bottomText}</div>
                 </Container>
             </>
         );
